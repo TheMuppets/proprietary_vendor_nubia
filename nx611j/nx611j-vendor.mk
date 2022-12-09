7d596e2be6ad23c3483aa93157e7c4fa1add5c61
daniml3 <daniel@danielml.dev>
2022-12-09 22:20:52 +0800

    nx611j: Drop QTI datastatusnotification
    * In some situations this app crashes due to a missing framework method on A12,
      causing network fluctuation with dual sim.
    
    * As it isn't needed, let's just remove it

--- nx611j/nx611j-vendor.mk
+++ nx611j/nx611j-vendor.mk
@@ -446,7 +446,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
